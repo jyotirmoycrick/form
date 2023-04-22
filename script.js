e9ec24933a600474661a7734453692cae3fc3e7a
jyotirmoycrick <112798691+jyotirmoycrick@users.noreply.github.com>
2023-04-22 23:23:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function login()
+{
+    
+    var jyoti =document.getElementById("name").value
+    again = "Chutiya banaya tumko 😂 " + jyoti
+    
+    document.getElementById("ham").innerHTML=again
+     
+}
